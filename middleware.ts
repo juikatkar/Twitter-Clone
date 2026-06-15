b86c736a688104e2dbfd3aee435400905eba9b5b
Juilee Katkar <juikatkar@gmail.com>
2026-06-15 12:17:55 +0200

    profile page route protect

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/home/:path*", "/post-tweet/:path*"],
+  matcher: ["/home/:path*", "/post-tweet/:path*", "/profile/:path*"],
 }
